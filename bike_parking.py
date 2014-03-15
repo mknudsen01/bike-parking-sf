21ab5fb078bc53694235c61391caea89178d1491
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-15 15:54:41 -0700

    Add params to the api query to ensure completion
    * Now query will only return those spots that are completed as
      well as have more than 0 spaces

--- bike_parking.py
+++ bike_parking.py
@@ -9,16 +9,11 @@ def hello():
 @app.route("/spots", methods=["GET"])
 def get_parking_spots():
   spots = obtain_all_spots()
-  print spots
   return spots
 
 def obtain_all_spots():
-  results = requests.get('https://data.sfgov.org/resource/w969-5mn4.json').json()
-
+  results = requests.get('https://data.sfgov.org/resource/w969-5mn4.json?status=COMPLETE&$where=spaces>0').json()
   return results
-
-
-
 
 if __name__ == '__main__':
   app.run()
