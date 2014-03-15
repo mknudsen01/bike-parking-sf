d1ebb5eeb4a37de8a3f5219444f591f45017f1ca
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-15 14:14:35 -0700

    Obtain all bike parking locations
    * Set a route '/spots' to obtain all parking spots
    * Spots obtained from https://data.sfgov.org/resource/w969-5mn4.json
    * Installed requests to make url requests simpler,
      which is at http://docs.python-requests.org/en/latest/
    
    Next steps:
    * Unfinished parking locations are included in this list, so it will
      need to be pared down to filter out those locations that are not
      currently usable

--- bike_parking.py
+++ bike_parking.py
@@ -1,9 +1,24 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+  return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/spots", methods=["GET"])
+def get_parking_spots():
+  spots = obtain_all_spots()
+  print spots
+  return spots
+
+def obtain_all_spots():
+  results = requests.get('https://data.sfgov.org/resource/w969-5mn4.json').json()
+
+  return results
+
+
+
+
+if __name__ == '__main__':
+  app.run()
